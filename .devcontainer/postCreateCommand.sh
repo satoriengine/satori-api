539174e1e589177b1a8aa5cefdb96aa796aee0f1
Hiroshi Fujiwara <hiroshi.829f@gmail.com>
2026-08-05 07:23:02 +0000

    update mise setting and version

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -20,8 +20,8 @@ else
   echo "miseをインストールします"
   sudo apt update -y && sudo apt install -y curl
   sudo install -dm 755 /etc/apt/keyrings
-  curl -fSs https://mise.jdx.dev/gpg-key.pub | sudo tee /etc/apt/keyrings/mise-archive-keyring.pub 1> /dev/null
-  echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.pub arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+  wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg > /dev/null
+  echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64,arm64,armhf] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list  
   sudo apt update
   sudo apt install -y mise
   # mise setting to bashrc
